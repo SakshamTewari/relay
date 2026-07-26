1c03831a3813a835fe6fb07e87f9bd8b0006620f
Saksham Tewari <sakshamtewari3@gmail.com>
2026-07-26 17:38:14 +0530

    update: register workspace routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,21 @@
-import Fastify from 'fastify';
+import Fastify from "fastify";
+import buildAuth from "./composition/auth";
+import { authRoutes } from "./routes/auth.route";
+import jwtPlugin from "./plugins/jwt.plugin";
+import { workspaceRoutes } from "./routes/workspace.route";
+import buildWorkspace from "./composition/workspace";
 
 const app = Fastify();
+
+const { authController } = buildAuth(app);
+const { workspaceController } = buildWorkspace();
+
+// JWT Plugin
+app.register(jwtPlugin);
+//Auth Routes
+app.register(authRoutes, {prefix: "/api/auth", authController});
+//Workspace Routes
+app.register(workspaceRoutes, {prefix: "api/workspaces", workspaceController});
+
 
 export default app;
