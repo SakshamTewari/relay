8160e217bf1a71a27dda18d4729d21e28e0da4c0
Saksham Tewari <sakshamtewari3@gmail.com>
2026-07-28 21:22:00 +0530

    add: channel types

---
+++ backend/src/types/channel.ts
@@ -0,0 +1,15 @@
+export interface CreatechannelRequest {
+    name: string;
+};
+
+export interface UpdateChannelRequest {
+    name: string;
+};
+
+export interface WorkspaceChannelParams {
+    workspaceId: string;
+};
+
+export interface ChannelParams {
+    channelId: string;
+}
