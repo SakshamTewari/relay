9de526b3d8d4cb2688ffb7ca7383dea3838b8a62
Saksham Tewari <sakshamtewari3@gmail.com>
2026-08-02 19:18:23 +0530

    Remove old frontend

--- frontend/convex/schema.ts
+++
@@ -1,9 +0,0 @@
-import { defineSchema, defineTable } from 'convex/server';
-import { authTables } from '@convex-dev/auth/server';
-
-const schema = defineSchema({
-  ...authTables,
-  // Other tables
-});
-
-export default schema;
