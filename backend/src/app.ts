7dc48c1d2133d3e50829f057e8ed84cbca6d57f2
Saksham Tewari <sakshamtewari3@gmail.com>
2026-08-05 21:00:04 +0530

    update: register channel routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,18 @@
 import Fastify from "fastify";
-import buildAuth from "./composition/auth";
-import { authRoutes } from "./routes/auth.route";
 import jwtPlugin from "./plugins/jwt.plugin";
-import { workspaceRoutes } from "./routes/workspace.route";
+import buildAuth from "./composition/auth";
 import buildWorkspace from "./composition/workspace";
+import buildChannel from "./composition/channel";
+import { authRoutes } from "./routes/auth.route";
+import { workspaceRoutes } from "./routes/workspace.route";
+import { channelRoutes } from "./routes/channel.route";
+
 
 const app = Fastify();
 
 const { authController } = buildAuth(app);
 const { workspaceController } = buildWorkspace();
+const { channelController } = buildChannel();
 
 // JWT Plugin
 app.register(jwtPlugin);
@@ -16,6 +20,7 @@ app.register(jwtPlugin);
 app.register(authRoutes, {prefix: "/api/auth", authController});
 //Workspace Routes
 app.register(workspaceRoutes, {prefix: "api/workspaces", workspaceController});
-
+//Channel Routes
+app.register(channelRoutes, {prefix: "api", channelController});
 
 export default app;
