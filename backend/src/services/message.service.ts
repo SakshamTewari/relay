530b730461c2244b271f31a6d23aa594cc439ec5
Saksham Tewari <sakshamtewari3@gmail.com>
2026-08-09 21:53:16 +0530

    add: message service

---
+++ backend/src/services/message.service.ts
@@ -0,0 +1,60 @@
+import { randomUUID } from "crypto";
+import { Message } from "../models/message.model";
+import { MessageRepository } from "../repositories/message.repository";
+import { ChannelRepository } from "../repositories/channel.repository";
+import { CreateMessageRequest, UpdateMessageRequest } from "../types/message";
+
+export class MessageService {
+
+    constructor(
+        private readonly messageRepository: MessageRepository,
+        private readonly channelRepository: ChannelRepository,
+    ){};
+
+    // Create Message
+    async createMessage(channelId: string, userId: string,request: CreateMessageRequest): Promise<Message> {
+        const channel = await this.channelRepository.findChannelById(channelId);
+        if(!channel) throw new Error("Channel not found");
+        const message: Message = {
+            id: randomUUID(),
+            channelId,
+            senderId : userId,
+            content: request.content,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        };
+        return await this.messageRepository.createMessage(message);
+    };
+
+    // Get All Messages by Channel ID
+    async getMessagesByChannelId(channelId: string): Promise<Message[]> {
+        const channel = await this.channelRepository.findChannelById(channelId);
+        if(!channel) throw new Error("Channel not found");
+        return await this.messageRepository.findMessagesByChannelId(channelId);
+
+    };
+
+    // Get Message by ID
+    async getMessageById(messageId: string): Promise<Message | null> {
+        const message = await this.messageRepository.findMessageById(messageId);
+        if (!message) return null;
+        return message;
+    };
+
+    // Update Message
+    async updateMessage(messageId: string, request: UpdateMessageRequest): Promise<Message | null> {
+
+        const message = await this.messageRepository.findMessageById(messageId);
+        if (!message) throw new Error("Message not found");
+
+        message.content = request.content;
+        message.updatedAt = new Date();
+
+        return await this.messageRepository.updateMessage(message);
+    };
+
+      // Delete Message
+    async deleteMessage(messageId: string): Promise<boolean> {
+        return await this.messageRepository.deleteMessage(messageId);
+    };
+}
