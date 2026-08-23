4729622b8dac0f53b3e3bf7fdc426ad7561fb357
Saksham Tewari <sakshamtewari3@gmail.com>
2026-08-23 16:15:22 +0530

    update: register workspace member routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,11 +4,12 @@ import buildAuth from "./composition/auth";
 import buildWorkspace from "./composition/workspace";
 import buildChannel from "./composition/channel";
 import buildMessage from "./composition/message";
+import buildWorkspaceMember from "./composition/workspace-member";
 import { authRoutes } from "./routes/auth.route";
 import { workspaceRoutes } from "./routes/workspace.route";
 import { channelRoutes } from "./routes/channel.route";
 import { messageRoutes } from "./routes/message.routes";
-
+import { workspaceMemberRoutes } from "./routes/workspace-member.route";
 
 const app = Fastify();
 
@@ -16,6 +17,7 @@ const { authController } = buildAuth(app);
 const { workspaceController } = buildWorkspace();
 const { channelController } = buildChannel();
 const { messageController } = buildMessage();
+const { workspaceMemberController } = buildWorkspaceMember();
 
 // JWT Plugin
 app.register(jwtPlugin);
@@ -27,5 +29,7 @@ app.register(workspaceRoutes, {prefix: "/api/workspaces", workspaceController});
 app.register(channelRoutes, {prefix: "/api", channelController});
 //Message Routes
 app.register(messageRoutes, {prefix: "/api", messageController});
+// Workspace Member Routes
+app.register(workspaceMemberRoutes, {prefix: "/api", workspaceMemberController});
 
 export default app;
