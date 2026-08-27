a15f0be77e2274f3eed0c211861d65447e708203
Saksham Tewari <sakshamtewari3@gmail.com>
2026-08-27 23:19:09 +0530

    update: protected

--- backend/src/routes/message.routes.ts
+++ backend/src/routes/message.routes.ts
@@ -6,27 +6,27 @@ import { CreateMessageSchema, UpdateMessageSchema } from "../schemas/message.sch
 export async function messageRoutes(app: FastifyInstance, { messageController }: { messageController: MessageController; },){
 
     // Create Message
-    app.post<{Params: ChannelMessageParams; Body: CreateMessageRequest}>("/channels/:channelId/messages", { schema: CreateMessageSchema }, async (request, reply) => {
+    app.post<{Params: ChannelMessageParams; Body: CreateMessageRequest}>("/channels/:channelId/messages", {onRequest: [app.authenticate], schema: CreateMessageSchema }, async (request, reply) => {
         return messageController.createMessage(request, reply);
     });
 
      // Get Messages by Channel ID
-    app.get<{Params: ChannelMessageParams}>("/channels/:channelId/messages", async (request, reply) => {
+    app.get<{Params: ChannelMessageParams}>("/channels/:channelId/messages", {onRequest: [app.authenticate]}, async (request, reply) => {
         return messageController.getMessagesByChannelId(request, reply);
     });
 
     // Get Message by ID
-    app.get<{Params: MessageParams}>("/messages/:messageId", async (request, reply) => {
+    app.get<{Params: MessageParams}>("/messages/:messageId", {onRequest: [app.authenticate]}, async (request, reply) => {
         return messageController.getMessageById(request, reply);
     });
 
     // Update Message
-    app.patch<{Params: MessageParams; Body: UpdateMessageRequest}>("/messages/:messageId", { schema: UpdateMessageSchema }, async (request, reply) => {
+    app.patch<{Params: MessageParams; Body: UpdateMessageRequest}>("/messages/:messageId", {onRequest: [app.authenticate], schema: UpdateMessageSchema }, async (request, reply) => {
             return messageController.updateMessage(request, reply);
     });
 
     // Delete Message
-    app.delete<{Params: MessageParams}>("/messages/:messageId", async (request, reply) => {
+    app.delete<{Params: MessageParams}>("/messages/:messageId", {onRequest: [app.authenticate]}, async (request, reply) => {
         return messageController.deleteMessage(request, reply);
     });
 };
