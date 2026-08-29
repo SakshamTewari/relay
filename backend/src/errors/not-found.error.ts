f968aad31899855c10e8209f4f7a47f60e3d4668
Saksham Tewari <sakshamtewari3@gmail.com>
2026-08-29 19:10:46 +0530

    update: code

--- backend/src/errors/not-found.error.ts
+++ backend/src/errors/not-found.error.ts
@@ -1,7 +1,7 @@
 import { AppError } from "./app.error";
 
 export class NotFoundError extends AppError {
-    constructor(message: string){
-        super(message, 404);
+    constructor(message: string, code: string){
+        super(message, 404, code);
     }
 }
