fe1bb8be0108c0d9a1e2123c661becba8abea308
Saksham Tewari <sakshamtewari3@gmail.com>
2026-09-08 20:21:50 +0530

    update: passing assistant service

--- backend/src/composition/assistant.ts
+++ backend/src/composition/assistant.ts
@@ -1,10 +1,14 @@
+import { LLMService } from "../infrastructure/ai/llm/llm.service";
 import { OpenAIService } from "../infrastructure/ai/llm/openai/openai.service";
+import { AssistantService } from "../services/assistant.service";
 
 export function buildAssistant(){
     
-    const llmService = new OpenAIService();
+    const llmService: LLMService = new OpenAIService();
+    const assistantService = new AssistantService(llmService);
+
 
     return {
-        llmService,
+        assistantService,
     }
 }
